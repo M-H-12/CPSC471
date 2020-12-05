c95828aaeef6d8c94482c0dea4d54c8837318860
JaydenBischoff <jayden.bischoff1@ucalgary.ca>
2020-12-05 12:04:17 -0700

    added classes to models.py

--- api/models.py
+++ api/models.py
@@ -1,5 +1,18 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
 
+class Staff(PolymorphicModel):
+    SIN = models.IntegerField(primary_key=True)
+    salary = models.IntegerField()
+    hired_date = models.DateField()
+
+    def json_data(self, **kwargs):
+        staff = {
+            'SIN': self.SIN,
+            'salary': self.salary,
+            'hired_date': self.hired_date
+        }
+        return staff
 
 class Counselor(Staff):
     counsels = models.ManyToManyField(Student, blank=True)
@@ -89,3 +102,45 @@ class Material(models.Model):
             'description': self.description.__str__()
         }
         return material
+
+class Course(models.Model):
+    course_id = models.IntegerField(primary_key=True)
+    course_name = models.CharField(max_length=20)
+    Prerequisite = models.ForeignKey('self', related_name='course', on_delete=models.CASCADE)
+
+    def json_data(self, **kwargs):
+        course = {
+            'course_id': self.course_id,
+            'course_name': self.course_name.__str__
+        }
+        return course
+
+class Offering(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    offering_no = models.IntegerField()
+    no_of_students = models.IntegerField()
+    room_no = models.IntegerField()
+
+    def json_data(self, **kwargs):
+        offering = {
+            'course_id': self.course_id,
+            'offering_no': self.offering_no,
+            'no_of_students': self.no_of_students,
+            'room_no': self.room_no
+        }
+        return offering
+
+class Offering_day_and_time(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    offering_no = models.IntegerField()
+    day = models.CharField()
+    time = models.CharField()
+
+    def json_data(self, **kwargs):
+        offering_day_and_time = {
+            'course_id': self.course_id,
+            'offering_no': self.offering_no,
+            'day': self.day.__str__,
+            'time':self.time.__str__
+        }
+        return offering_day_and_time
