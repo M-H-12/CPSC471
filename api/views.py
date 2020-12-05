b2ad0220dfb0b42cba94e83f07478838a690e9dc
Bryce <brycepcayanan@gmail.com>
2020-12-05 15:18:35 -0700

    Included some files

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+import json
+from . import models
 
 # Create your views here.
+
+def course_data(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+    #return JsonResponse()
+
+def create_course(request):
+    if request.method != "POST":
+        response = JsonResponse({'error': 'This endpoint can only be accessed with POST'})
+        response.status_code = 400
+        return response
+
+    content = json.loads(request.body)['content']
