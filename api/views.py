d652269a953c6452754b3e2ebf8f9f00b80865c2
Bryce Cayanan <65361192+bcayanan@users.noreply.github.com>
2020-12-06 11:55:36 -0700

    Update views.py

--- api/views.py
+++ api/views.py
@@ -155,20 +155,6 @@ def prerequisite(request):
     return response
 
 
-def course_data(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-    #return JsonResponse()
-
-
-def create_course(request):
-    if request.method != "POST":
-        response = JsonResponse({'error': 'This endpoint can only be accessed with POST'})
-        response.status_code = 400
-        return response
-
-    content = json.loads(request.body)['content']
-
-
 def student():
     inputInfo = json.loads(request.body)['content']
     if request.method == "GET":
